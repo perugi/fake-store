2242d304d74fe179d1537d33c9322dc6f41f3021
perugi <dominik.perusko@gmail.com>
2025-12-29 23:36:14 +0100

    Add loading and error state

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,42 +6,54 @@ import styles from "./Home.module.css";
 import products from "../../data/products.json";
 import { useEffect } from "react";
 
-const TRENDING_IDS = [1, 2, 3, 4, 5]
+const TRENDING_IDS = [1, 2, 3, 4, 5];
 
 function Home() {
   const [trendingProducts, setTrendingProducts] = useState([]);
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     Promise.all(
-      TRENDING_IDS.map(id =>
-        fetch(`https://fakestoreapi.com/products/${id}`)
-          .then(r => {
-            if (!r.ok) throw new Error(`HTTP ${r.status}`);
-            return r.json();
-          })
+      TRENDING_IDS.map((id) =>
+        fetch(`https://fakestoreapi.com/product/${id}`).then((r) => {
+          if (!r.ok) throw new Error(`HTTP ${r.status}`);
+          return r.json();
+        })
       )
     )
-      .then(products => {
+      .then((products) => {
         setTrendingProducts(products);
-        console.log(products);
       })
-      .catch(err => {
-        console.error(err);
+      .catch((err) => {
+        setError(err);
+        console.error("Error fetching trending products:", {
+          message: err.message,
+          name: err.name,
+          stack: err.stack,
+        });
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
-  
+
   return (
     <div>
       Home
       <Link to="/shop">
-        <Button label="Click Me" variant="secondary"/>
+        <Button label="Click Me" variant="secondary" />
       </Link>
       <section className={styles.trendingItems}>
-        {trendingProducts.map(product => (
-          <ItemCard key={product.id} product={product} />
-        ))}
+        {error ? (
+          <p>Error loading products: {error.message}</p>
+        ) : loading ? (
+          <p>Loading...</p>
+        ) : (
+          trendingProducts.map((product) => (
+            <ItemCard key={product.id} product={product} />
+          ))
+        )}
       </section>
     </div>
   );
